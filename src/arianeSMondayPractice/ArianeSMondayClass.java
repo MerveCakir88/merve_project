8625c631ce8b42181e1d56ad586bcb69614e7fbe
Hakan Gok <hakangok1974@yahoo.com>
2022-04-25 12:27:12 +0100

    3rd commit

--- src/arianeSMondayPractice/ArianeSMondayClass.java
+++ src/arianeSMondayPractice/ArianeSMondayClass.java
@@ -3,4 +3,8 @@ package arianeSMondayPractice;
 public class ArianeSMondayClass {
     //initial commit
     //second commit
+
+    public static void main(String[] args) {
+        System.out.println("basic print code");
+    }
 }

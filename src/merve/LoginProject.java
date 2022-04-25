672b29da465ada48a09c9940af4f19ed6d09faf7
mervecakir <m.cakir88w@gmail.com>
2022-04-25 12:28:27 +0100

    very execited!

--- src/merve/LoginProject.java
+++ src/merve/LoginProject.java
@@ -3,5 +3,7 @@ package merve;
 public class LoginProject {
     public static void main(String[] args) {
         System.out.println("we are creating project");
+
+        System.out.println("everyone is very execited!");
     }
 }

0915b98f0970f17f853c916ecd64e166bfde0b01
ulkucardak <ulkucardak@gmail.com>
2022-04-25 12:28:09 +0100

    Login project started

---
+++ src/ulku_works/tc001_Ulku.java
@@ -0,0 +1,12 @@
+package ulku_works;
+
+public class tc001_Ulku {
+    public static void main(String[] args) {
+
+        System.out.println(" I am testing tc001 for Login project");
+    }
+
+
+
+
+}
